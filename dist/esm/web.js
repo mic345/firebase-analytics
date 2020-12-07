f163ee7ef414aad61b1ed033fb679fc64b3db2ae
Mickey Mokotov <m.mokotov@gmail.com>
2020-12-07 15:53:43 -0600

    added dist directory

---
+++ dist/esm/web.js
@@ -0,0 +1,258 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { WebPlugin } from "@capacitor/core";
+export class FirebaseAnalyticsWeb extends WebPlugin {
+    constructor() {
+        super({
+            name: "FirebaseAnalytics",
+            platforms: ["web"],
+        });
+        this.not_supported_mssg = "This method is not supported";
+        this.options_missing_mssg = "Firebase options are missing";
+        this.analytics_missing_mssg = "Firebase analytics is not initialized. Make sure initializeFirebase() is called once";
+        this.scripts = [
+            {
+                key: "firebase-app",
+                src: "https://www.gstatic.com/firebasejs/7.15.4/firebase-app.js",
+            },
+            {
+                key: "firebase-ac",
+                src: "https://www.gstatic.com/firebasejs/7.15.4/firebase-analytics.js",
+            },
+        ];
+        this.ready = new Promise((resolve) => (this.readyResolver = resolve));
+        this.loadScripts();
+    }
+    /**
+     * Sets the user ID property.
+     * @param options - userId: unique identifier of the user to log
+     * Platform: Web/Android/iOS
+     */
+    setUserId(options) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield this.ready;
+            if (!this.analyticsRef) {
+                reject(this.analytics_missing_mssg);
+                return;
+            }
+            const { userId } = options || { userId: undefined };
+            if (!userId) {
+                reject("userId property is missing");
+                return;
+            }
+            this.analyticsRef.setUserId(userId);
+            resolve();
+        }));
+    }
+    /**
+     * Sets a user property to a given value.
+     * @param options - name: The name of the user property to set.
+     *                  value: The value of the user property.
+     * Platform: Web/Android/iOS
+     */
+    setUserProperty(options) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield this.ready;
+            if (!this.analyticsRef) {
+                reject(this.analytics_missing_mssg);
+                return;
+            }
+            const { name, value } = options || { name: undefined, value: undefined };
+            if (!name) {
+                reject("name property is missing");
+                return;
+            }
+            if (!value) {
+                reject("value property is missing");
+                return;
+            }
+            let property = {};
+            property[name] = value;
+            this.analyticsRef.setUserProperties(property);
+            resolve();
+        }));
+    }
+    /**
+     * Retrieves the app instance id from the service.
+     * @returns - instanceId: current instance if of the app
+     * Platform: Web/Android/iOS
+     */
+    getAppInstanceId() {
+        return new Promise((resolve, _reject) => resolve);
+    }
+    /**
+     * Sets the current screen name, which specifies the current visual context in your app.
+     * @param options - screenName: the activity to which the screen name and class name apply.
+     *                  nameOverride: the name of the current screen. Set to null to clear the current screen name.
+     * Platform: Android/iOS
+     */
+    setScreenName(_options) {
+        return new Promise((resolve, _reject) => resolve);
+    }
+    /**
+     * Clears all analytics data for this app from the device and resets the app instance id.
+     * Platform: Android/iOS
+     */
+    reset() {
+        return new Promise((resolve, _reject) => resolve);
+    }
+    /**
+     * Logs an app event.
+     * @param options - name: unique name of the event
+     *                  params: the map of event parameters.
+     * Platform: Web/Android/iOS
+     */
+    logEvent(options) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield this.ready;
+            if (!this.analyticsRef) {
+                reject(this.analytics_missing_mssg);
+                return;
+            }
+            const { name, params } = options || {
+                name: undefined,
+                params: undefined,
+            };
+            if (!name) {
+                reject("name property is missing");
+                return;
+            }
+            this.analyticsRef.logEvent(name, params);
+            resolve();
+        }));
+    }
+    /**
+     * Sets whether analytics collection is enabled for this app on this device.
+     * @param options - enabled: boolean true/false to enable/disable logging
+     * Platform: Web/Android/iOS
+     */
+    setCollectionEnabled(options) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield this.ready;
+            if (!this.analyticsRef) {
+                reject(this.analytics_missing_mssg);
+                return;
+            }
+            const { enabled } = options || { enabled: false };
+            this.analyticsRef.setAnalyticsCollectionEnabled(enabled);
+            resolve();
+        }));
+    }
+    /**
+     * Sets the duration of inactivity that terminates the current session.
+     * @param options - duration: duration of inactivity
+     * Platform: Android/iOS
+     */
+    setSessionTimeoutDuration(_options) {
+        return new Promise((_resolve, reject) => {
+            reject(this.not_supported_mssg);
+        });
+    }
+    /**
+     * Returns analytics reference object
+     */
+    get remoteConfig() {
+        return this.analyticsRef;
+    }
+    enable() {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield this.ready;
+            if (!this.analyticsRef) {
+                reject(this.analytics_missing_mssg);
+                return;
+            }
+            this.analyticsRef.setAnalyticsCollectionEnabled(true);
+            resolve();
+        }));
+    }
+    disable() {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield this.ready;
+            if (!this.analyticsRef) {
+                reject(this.analytics_missing_mssg);
+                return;
+            }
+            this.analyticsRef.setAnalyticsCollectionEnabled(false);
+            resolve();
+        }));
+    }
+    // 
+    // Note: The methods below are common to all Firebase capacitor plugins. Best to create `capacitor-community / firebase-common`,
+    // move the code there and add it as module to all FB plugins.
+    // 
+    /**
+     * Configure and Initialize FirebaseApp if not present
+     * @param options - web app's Firebase configuration
+     * @returns firebase analytics object reference
+     * Platform: Web
+     */
+    initializeFirebase(options) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!options)
+                throw new Error(this.options_missing_mssg);
+            yield this.firebaseObjectReadyPromise();
+            const app = this.isFirebaseInitialized() ? window.firebase : window.firebase.initializeApp(options);
+            this.analyticsRef = app.analytics();
+            this.readyResolver();
+            return this.analyticsRef;
+        });
+    }
+    /**
+     * Check for existing loaded script and load new scripts
+     */
+    loadScripts() {
+        return Promise.all(this.scripts.map(s => this.loadScript(s.key, s.src)));
+    }
+    /**
+     * Loaded single script with provided id and source
+     * @param id - unique identifier of the script
+     * @param src - source of the script
+     */
+    loadScript(id, src) {
+        return new Promise((resolve, reject) => {
+            if (document.getElementById(id)) {
+                resolve(null);
+            }
+            else {
+                const file = document.createElement("script");
+                file.type = "text/javascript";
+                file.src = src;
+                file.id = id;
+                file.onload = resolve;
+                file.onerror = reject;
+                document.querySelector("head").appendChild(file);
+            }
+        });
+    }
+    firebaseObjectReadyPromise() {
+        var tries = 100;
+        return new Promise((resolve, reject) => {
+            const interval = setInterval(() => {
+                if (window.firebase) {
+                    clearInterval(interval);
+                    resolve(null);
+                }
+                else if (tries-- <= 0) {
+                    reject("Firebase fails to load");
+                }
+            }, 50);
+        });
+    }
+    isFirebaseInitialized() {
+        var _a, _b;
+        const length = (_b = (_a = window.firebase) === null || _a === void 0 ? void 0 : _a.apps) === null || _b === void 0 ? void 0 : _b.length;
+        return length && length > 0;
+    }
+}
+const FirebaseAnalytics = new FirebaseAnalyticsWeb();
+export { FirebaseAnalytics };
+import { registerWebPlugin } from "@capacitor/core";
+registerWebPlugin(FirebaseAnalytics);
+//# sourceMappingURL=web.js.map
